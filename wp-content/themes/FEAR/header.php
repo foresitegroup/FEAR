8efcc2b25c9eaec205d5e658a7067d8e861ac070
foresitegroup <mark@foresitegrp.com>
2016-01-06 15:41:06 -0600

    Add Google Analytics

--- wp-content/themes/FEAR/header.php
+++ wp-content/themes/FEAR/header.php
@@ -26,6 +26,17 @@
       jQuery("a[href^='http'], a[href$='.pdf']").not("[href*='" + window.location.host + "']").attr('target','_blank');
     });
   </script>
+  <!-- BEGIN Google Analytics -->
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-72109900-1', 'auto');
+    ga('send', 'pageview');
+  </script>
+  <!-- END Google Analytics -->
 </head>
 
 <body>
